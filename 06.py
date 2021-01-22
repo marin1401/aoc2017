735fa9992d24a1b5a2fa88033dc51cbadb322af5
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-22 11:27:40 +0100

    Update 06.py

--- 06.py
+++ 06.py
@@ -5,31 +5,25 @@ with open('./06.txt') as myinput:
 
 memory_banks = [int(memory_bank) for memory_bank in memory_banks]
 
-states = set()
-counter, seen_state_cycle = 0, 0
+configuration = tuple(memory_banks)
+states = [configuration]
+states_set = {configuration}
+counter = 0
 while True:
+    counter += 1
     position = memory_banks.index(max(memory_banks))
     blocks = memory_banks[position]
     memory_banks[position] = 0
     for _ in range(blocks):
-        position = 0 if position == len(memory_banks) - 1 else position + 1
-        memory_banks[position] += 1
+        position += 1
+        memory_banks[position % len(memory_banks)] += 1
     configuration = tuple(memory_banks)
-    if configuration in states:
-        if not seen_state_cycle:
-            seen_state_cycle = counter + 1
-            states = {configuration}
-        else:
-            counter -= seen_state_cycle - 1
-            break
+    if configuration in states_set:
+        #Part 1
+        print(counter)
+        #Part 2
+        print(counter - states.index(configuration))
+        break
     else:
-        states.add(configuration)
-    counter += 1
-
-#Part 1
-
-print(seen_state_cycle)
-
-#Part 2
-
-print(counter)
+        states.append(configuration)
+        states_set.add(configuration)
